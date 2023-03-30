f48955919bb8f1bd94f74b6d44a68e9ddb2f540f
GitHub Action <action@github.com>
2023-03-30 10:08:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/jackjonson/windicssintellisenceplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/jackjonson/windicssintellisenceplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.jackjonson.windicssintellisenceplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jackjonson.windicssintellisenceplugin.MyBundle
+import com.github.jackjonson.windicssintellisenceplugin.services.MyProjectService
 import javax.swing.JButton
 
 
